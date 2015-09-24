6c26bd82e99c0097b8e0187b3edcd198b364d908
Barney Boisvert <bboisvert@gmail.com>
2015-09-24 07:54:49 -0700

    pulled a 'baseModel' to use as a basis for the various persistent structs

--- model/model.go
+++ model/model.go
@@ -4,27 +4,27 @@ import (
 	"time"
 )
 
-type User struct {
+type baseModel struct {
 	ID        int64 `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Name      string
-	Username  string
-	Passhash  string
-	Events    []Event
+}
+
+type User struct {
+	baseModel
+	Name     string
+	Username string
+	Passhash string
+	Events   []Event
 }
 
 type Tag struct {
-	ID        int64 `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Tag       string
+	baseModel
+	Tag string
 }
 
 type Event struct {
-	ID        int64 `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	baseModel
 	UserID    int64 `sql:"index"`
 	Timestamp time.Time
 	Tags      []Tag `gorm:"many2many:event_tags;"`

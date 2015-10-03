ee247e85d88c72a868a38d592b4c42fc276b2dc2
Barney Boisvert <bboisvert@gmail.com>
2015-10-03 12:52:32 -0700

    removed embeddable structs, as they don't appear to wire up correctly when reading from the DB

--- model/model.go
+++ model/model.go
@@ -9,33 +9,27 @@ import (
 )
 
 /*
-baseModel is a struct that declares the core meta fields that all persistent
-structs will have via Go's implicit key delegation.
-*/
-type baseModel struct {
-	ID        int64 `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-}
-
-/*
 User represents a user of datawell which owns events and tags.
 */
 type User struct {
-	baseModel
-	Name     string
-	Username string
-	Passhash string
-	Events   []Event
+	ID        int64 `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Username  string
+	Passhash  string
+	Events    []Event
 }
 
 /*
 Tag represents a single tag in datawell, which are owned by a single user.
 */
 type Tag struct {
-	baseModel
-	UserID int64
-	Tag    string
+	ID        int64 `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    int64
+	Tag       string
 }
 
 /*
@@ -43,7 +37,9 @@ Event represents an event that was recorded in datawell, which are owned by a
 single user.
 */
 type Event struct {
-	baseModel
+	ID        int64 `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 	UserID    int64
 	Timestamp time.Time
 	Tags      []EventTag
@@ -51,19 +47,13 @@ type Event struct {
 }
 
 /*
-tagRef is a struct for references of a tag from "something" else, which will be
-expressed as embedding structs.
-*/
-type tagRef struct {
-	baseModel
-	Tag    Tag
-	Number sql.NullFloat64
-}
-
-/*
 EventTag represents a tag that was added to an event, optionally with a number.
 */
 type EventTag struct {
-	tagRef
-	Event Event
+	ID        int64 `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Event     Event
+	Tag       Tag
+	Number    sql.NullFloat64
 }

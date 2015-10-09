1b3f40b824031f38e021bb3f6efdb6d8af4f67ca
Barney Boisvert <bboisvert@gmail.com>
2015-10-09 12:02:04 -0700

    created a list-users command

---
+++ cmd_list_users.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/folded-ear/datawell/model"
+	"log"
+	"strconv"
+)
+
+var (
+	listUsersCmd = &Command{
+		Name:    "list-users",
+		Usage:   "",
+		Summary: "list all users in the system",
+		Run:     listUsersRun,
+	}
+)
+
+func listUsersRun(cmd *Command, args ...string) {
+	users := []model.User{}
+	db, err := model.Gorm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Order("name").Order("username").Find(&users).Error
+	if err != nil {
+		log.Fatal(err)
+	}
+	idLen, nameLen, unLen := 2, 4, 8
+	for _, u := range users {
+		l := len(strconv.FormatInt(u.ID, 10))
+		if l > idLen {
+			idLen = l
+		}
+		l = len(u.Name)
+		if l > nameLen {
+			nameLen = l
+		}
+		l = len(u.Username)
+		if l > unLen {
+			unLen = l
+		}
+	}
+	fmt.Printf("| %*s | %-*s | %-*s |\n", idLen, "id", nameLen, "name", unLen, "username")
+	for _, u := range users {
+		fmt.Printf("| %*d | %-*s | %-*s |\n", idLen, u.ID, nameLen, u.Name, unLen, u.Username)
+	}
+	fmt.Printf("%d user(s)\n", len(users))
+}
